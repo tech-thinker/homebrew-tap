946dcb52343c41e020b524309b0ada650ea2f980
Asif Mohammad Mollah <admin@mrasif.in>
2024-09-26 11:04:22 +0400

    chatz v1.1.2 installation from pre-built binaries

--- chatz.rb
+++ chatz.rb
@@ -1,19 +1,42 @@
 class Chatz < Formula
-  desc "Slack, Google Chat, Discord and Telegram bot for sending messages/notifications."
+  desc "chatz is a versatile messaging app designed to send notifications to Google Chat, Slack, Discord, Telegram and Redis."
   homepage "https://github.com/tech-thinker/chatz"
-  url "https://github.com/tech-thinker/chatz/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "ead73999bc30b74163b957c2135904db660f72d32754cf44ee9ed75ef95c68ff"
   license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-darwin-arm64.tar.gz"
+      sha256 "e2ee4ccc808f13bc21fea8e8c71980d30a9066c629cbb86d73fa93b67cf96d1b"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-darwin-amd64.tar.gz"
+      sha256 "d3fdd464352b31207602e13dcd5c4c0aa6a2707b5dff5bbc9844ee7370ca2ff9"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_32_bit?
+        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-arm.tar.gz"
+        sha256 "1223cc0c34f488250f0cbea7d0fc261233e5cbe7d3f44c7dcafb9b18dd5aab8b"
+      else
+        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-arm64.tar.gz"
+        sha256 "b5a945b6cb67664e9f484972b8860a52d4f003c96fdb1a42da651ab45fe2fdae"
+      end
+    elsif Hardware::CPU.intel?
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-amd64.tar.gz"
+      sha256 "1b9ffd769454c187d0e25dac4ac04524382231266209a6293c4f34d977196284"
+    end
+  end
 
   depends_on "go" => :build
 
   def install
     # tag = version.to_s
     # commit = Utils.git_short_head
-    tag = "v1.1.2"
-    commit = "none"
-    system "make", "build", "VERSION=#{tag}", "COMMIT=#{commit}"
-    bin.install "chatz"
+    # tag = "v1.1.2"
+    # commit = "none"
+    # system "make", "build", "VERSION=#{tag}", "COMMIT=#{commit}"
+    bin.install "build/chatz"
     man1.install "man/chatz.1"
   end
 
