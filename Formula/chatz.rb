6b0c43e01e8a94346702cc77e11aab0670c6a1ff
Asif Mohammad Mollah <admin@mrasif.in>
2024-10-08 10:17:49 +0400

    Chatz v1.1.3 released

--- Formula/chatz.rb
+++ Formula/chatz.rb
@@ -1,14 +1,15 @@
 class Chatz < Formula
   desc "chatz is a versatile messaging app designed to send notifications to Google Chat, Slack, Discord, Telegram and Redis."
   homepage "https://github.com/tech-thinker/chatz"
+  version "v1.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-darwin-arm64.tar.gz"
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.3/chatz-darwin-arm64.tar.gz"
       sha256 "e2ee4ccc808f13bc21fea8e8c71980d30a9066c629cbb86d73fa93b67cf96d1b"
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-darwin-amd64.tar.gz"
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.3/chatz-darwin-amd64.tar.gz"
       sha256 "d3fdd464352b31207602e13dcd5c4c0aa6a2707b5dff5bbc9844ee7370ca2ff9"
     end
   end
@@ -16,14 +17,14 @@ class Chatz < Formula
   on_linux do
     if Hardware::CPU.arm?
       if Hardware::CPU.is_32_bit?
-        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-arm.tar.gz"
+        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.3/chatz-linux-arm.tar.gz"
         sha256 "1223cc0c34f488250f0cbea7d0fc261233e5cbe7d3f44c7dcafb9b18dd5aab8b"
       else
-        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-arm64.tar.gz"
+        url "https://github.com/tech-thinker/chatz/releases/download/v1.1.3/chatz-linux-arm64.tar.gz"
         sha256 "b5a945b6cb67664e9f484972b8860a52d4f003c96fdb1a42da651ab45fe2fdae"
       end
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.2/chatz-linux-amd64.tar.gz"
+      url "https://github.com/tech-thinker/chatz/releases/download/v1.1.3/chatz-linux-amd64.tar.gz"
       sha256 "1b9ffd769454c187d0e25dac4ac04524382231266209a6293c4f34d977196284"
     end
   end
