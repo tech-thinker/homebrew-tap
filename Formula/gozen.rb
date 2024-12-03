3a52326b77452e1ef6c52edf9a045eee426946ec
Asif Mohammad Mollah <admin@mrasif.in>
2024-12-03 18:57:09 +0400

    gozen v0.1.3 released (#10)

--- Formula/gozen.rb
+++ Formula/gozen.rb
@@ -1,31 +1,31 @@
 class Gozen < Formula
   desc "gozen is a simplified golang MVC framework to generate projects and components."
   homepage "https://github.com/tech-thinker/gozen"
-  version "v0.1.1"
+  version "v0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.2/gozen-darwin-arm64.tar.gz"
-      sha256 "d621a5f6a1075be282eb86e1e585833a1b13d6e62fb03beb6c5ee9289672532a"
+      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.3/gozen-darwin-arm64.tar.gz"
+      sha256 "d7f7cc8c13404cdb0c17fbb8c598daa97155f99130195e4bfb9b0b4fe96b8195"
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.2/gozen-darwin-amd64.tar.gz"
-      sha256 "4ab29ca47eb895b73061fa91b364e3db4b76d3815e23b75cf939d67641d565cf"
+      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.3/gozen-darwin-amd64.tar.gz"
+      sha256 "a475233a2560919fff26643f1cf77f27f1d1621dc2f0a85faab53063659edc6c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       if Hardware::CPU.is_32_bit?
-        url "https://github.com/tech-thinker/gozen/releases/download/v0.1.2/gozen-linux-arm.tar.gz"
-        sha256 "21a117ae69d701ecdc671609e585fd092b7d3526487e33adc83f188a552a0d53"
+        url "https://github.com/tech-thinker/gozen/releases/download/v0.1.3/gozen-linux-arm.tar.gz"
+        sha256 "7f1b04789c5401dd6fc8b826e0cdf16f4847522bb6f0e70e8389bbed9b3af59e"
       else
-        url "https://github.com/tech-thinker/gozen/releases/download/v0.1.2/gozen-linux-arm64.tar.gz"
-        sha256 "3640f612745bfd42dc57c6727b29d036467a50adfdacb8121595dc3468e72bba"
+        url "https://github.com/tech-thinker/gozen/releases/download/v0.1.3/gozen-linux-arm64.tar.gz"
+        sha256 "6ba29dcc64172b83297f87791c309c0e3e2ffb533d0521ee424f7356afb23cc2"
       end
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.2/gozen-linux-amd64.tar.gz"
-      sha256 "76106e3fe5539a564e49cb42ab26d8de1c8a8c49053c094ed0885479b4e98915"
+      url "https://github.com/tech-thinker/gozen/releases/download/v0.1.3/gozen-linux-amd64.tar.gz"
+      sha256 "dc266d743a9871203445a77a128b9829ac2da0512d23f2dad0214c6bcfbeccbf"
     end
   end
 
