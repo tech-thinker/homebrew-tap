a065d87bbfc4f3cb1bea71aaf40089ca3c6cff8a
Asif Mohammad Mollah <admin@mrasif.in>
2024-12-03 05:40:09 +0400

    gozen build distribution fixes

--- Formula/gozen.rb
+++ Formula/gozen.rb
@@ -7,10 +7,10 @@ class Gozen < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tech-thinker/gozen/releases/download/v0.1.0/gozen-darwin-arm64.tar.gz"
-      sha256 "9b53587d9f60b08ba7992fd2f27702547d33096de685c98524637510c191c4ed"
+      sha256 "4ae79c4ca941a7f1dc1a294f155f3d1fac391c8a302d4fc1011974bd0b3fe940"
     elsif Hardware::CPU.intel?
       url "https://github.com/tech-thinker/gozen/releases/download/v0.1.0/gozen-darwin-amd64.tar.gz"
-      sha256 "81388b92843260f8211c8e5be582211e0d1ca131c21445440647da49aff4d0c2"
+      sha256 "e2f7bc47051b0bcd8c4904590a3e660fe08ae3837f577ca3348116365fb6fb3b"
     end
   end
 
@@ -18,14 +18,14 @@ class Gozen < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_32_bit?
         url "https://github.com/tech-thinker/gozen/releases/download/v0.1.0/gozen-linux-arm.tar.gz"
-        sha256 "c4cedf9b7fdd48a4a79fc3c8d4d448baf1688a454aff36ff95a0ff26e66eff40"
+        sha256 "32247d76b40543e9fc13a6e251a08ef8dbaefa7153aa02fb6f644bf9b1d692a5"
       else
         url "https://github.com/tech-thinker/gozen/releases/download/v0.1.0/gozen-linux-arm64.tar.gz"
-        sha256 "303e36122a8254ec00d44225c26fd436f140d4ccdffc967210703672261ae601"
+        sha256 "4ae79c4ca941a7f1dc1a294f155f3d1fac391c8a302d4fc1011974bd0b3fe940"
       end
     elsif Hardware::CPU.intel?
       url "https://github.com/tech-thinker/gozen/releases/download/v0.1.0/gozen-linux-amd64.tar.gz"
-      sha256 "a033fd2e2054710730793ca4d62eebd44dda1c64705f27f3246a68809ae1fd5d"
+      sha256 "e2f7bc47051b0bcd8c4904590a3e660fe08ae3837f577ca3348116365fb6fb3b"
     end
   end
 
