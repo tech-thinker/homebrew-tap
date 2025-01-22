4f8908473b183167398e78322ba3ef42c1d437fe
Asif Mohammad Mollah <admin@mrasif.in>
2025-01-23 01:09:57 +0400

    telepath release v1.0.1

--- Formula/telepath.rb
+++ Formula/telepath.rb
@@ -1,31 +1,31 @@
 class Telepath < Formula
   desc "telepath is a powerful CLI tool for secure port forwarding with support for multiple jump hosts and flexible authentication."
   homepage "https://github.com/tech-thinker/telepath"
-  version "GPL-3.0-only"
-  license "MIT"
+  version "v1.0.1"
+  license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.0/telepath-darwin-arm64.tar.gz"
-      sha256 "8d17a55fdf55916d57327a3da4fbff138d7149b14e3e4d5a06b0043537d1af34"
+      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.1/telepath-darwin-arm64.tar.gz"
+      sha256 "7aaea816ab91b193b3703f8e470a03164deb1a4cc50a126aa8fad23a43551047"
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.0/telepath-darwin-amd64.tar.gz"
-      sha256 "461da42d3b510935b691e42a9e72b44f32b743754a7eeae2ce197638901acad2"
+      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.1/telepath-darwin-amd64.tar.gz"
+      sha256 "f6a0984c4a3e52948cca18ab0be9e74cefe2b45c6855344df1a17341b5ed8129"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       if Hardware::CPU.is_32_bit?
-        url "https://github.com/tech-thinker/telepath/releases/download/v1.0.0/telepath-linux-arm.tar.gz"
-        sha256 "f6926a1a22981d98c3836fb43ef62fac277801d48d534211001b63e13d4312fe"
+        url "https://github.com/tech-thinker/telepath/releases/download/v1.0.1/telepath-linux-arm.tar.gz"
+        sha256 "0110f41ea2f68c083dcf8e5ae85cca84fe52861eba7a5a0e0405260378522f89"
       else
-        url "https://github.com/tech-thinker/telepath/releases/download/v1.0.0/telepath-linux-arm64.tar.gz"
-        sha256 "3c7ed4165352385748b71ed14609da07b2379108df75462da800843ee9917652"
+        url "https://github.com/tech-thinker/telepath/releases/download/v1.0.1/telepath-linux-arm64.tar.gz"
+        sha256 "45a2d36b960c534f549742d7454de6010facc1fc99e2de350d4cfdaea9aef8d0"
       end
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.0/telepath-linux-amd64.tar.gz"
-      sha256 "62166b5660de8c2440c471454e239dc9132afdd549654113a6363228d07c8ed8"
+      url "https://github.com/tech-thinker/telepath/releases/download/v1.0.1/telepath-linux-amd64.tar.gz"
+      sha256 "e86019f3b567bd026375e54f06fc82cbdeb0a7f600f1f0f3693c2656bc216557"
     end
   end
 
