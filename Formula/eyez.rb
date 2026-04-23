01648d93661564ca5c0164f9a4cc4bbbe2c244d7
Asif Mohammad Mollah <admin@mrasif.in>
2026-04-23 21:36:59 +0400

    release eyez v1.4.0

--- Formula/eyez.rb
+++ Formula/eyez.rb
@@ -1,31 +1,31 @@
 class Eyez < Formula
   desc "eyez - is the powerful CLI tool for viewing image in the terminal (Mostly any terminal)."
   homepage "https://github.com/tech-thinker/eyez"
-  version "v1.3.0"
+  version "v1.4.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tech-thinker/eyez/releases/download/v1.3.0/eyez-darwin-arm64.tar.gz"
-      sha256 "768f32f4e24418c820f910e6d849e2b2f440b96539a5cd4a24e23582ea1bb822"
+      url "https://github.com/tech-thinker/eyez/releases/download/v1.4.0/eyez-darwin-arm64.tar.gz"
+      sha256 "da1e9f728dd3afcbc9a3b4e06074e973f827762d4646e3de884508dabfac8db6"
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/eyez/releases/download/v1.3.0/eyez-darwin-amd64.tar.gz"
-      sha256 "c21a3ee086546e6dafdf613baa5c740e272bc7f2ddea0f71967d13a6c9151775"
+      url "https://github.com/tech-thinker/eyez/releases/download/v1.4.0/eyez-darwin-amd64.tar.gz"
+      sha256 "42e387d74b2a0e098e200b663729eb7e2d26974375e9641b24c849f402d5abc3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       if Hardware::CPU.is_32_bit?
-        url "https://github.com/tech-thinker/eyez/releases/download/v1.3.0/eyez-linux-arm.tar.gz"
-        sha256 "72e7ff003f1d4159750463634d373cb2a3e695ae69307705eec047cf42e209b6"
+        url "https://github.com/tech-thinker/eyez/releases/download/v1.4.0/eyez-linux-arm.tar.gz"
+        sha256 "093a42edea12317aaefe8e434fe380368b8ebd600a2a938ed1cff44d5155bc8f"
       else
-        url "https://github.com/tech-thinker/eyez/releases/download/v1.3.0/eyez-linux-arm64.tar.gz"
-        sha256 "86f331531a4fb21bbde168c96b341d9d0923d522cfe37f0cb727e74332a65a47"
+        url "https://github.com/tech-thinker/eyez/releases/download/v1.4.0/eyez-linux-arm64.tar.gz"
+        sha256 "9b84afc45e9431ef4c0fbe63dca81b2c7308a35f70826adb00e9a8ce7a876a89"
       end
     elsif Hardware::CPU.intel?
-      url "https://github.com/tech-thinker/eyez/releases/download/v1.3.0/eyez-linux-amd64.tar.gz"
-      sha256 "92d0e3de3709a33e00e170fd1f4a49e266c77862be7676b9113b9f86d81b42db"
+      url "https://github.com/tech-thinker/eyez/releases/download/v1.4.0/eyez-linux-amd64.tar.gz"
+      sha256 "54e287ffbfa504cb1b302a048ff58f0a0f7b76eae30c12b594addc7bb067dad3"
     end
   end
 
